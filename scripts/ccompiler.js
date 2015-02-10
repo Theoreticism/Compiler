2c0092f0618a8c2aef0653d02112d79a7a91676e
chriskwlee <ckwl.default@gmail.com>
2015-02-10 12:06:22 -0500

    Labs 1 and 2
    Lab 2 is incomplete, questions about lambda transitions for NFA states.
    Minor changes to compiler base and lexer scripts.

--- scripts/ccompiler.js
+++ scripts/ccompiler.js
@@ -1,15 +1,40 @@
+//TODO: Verbose?
+
 var input;
 var tokens;
 var cst;
 var ast;
 var environ;
 
+/**
+ * Initializes the compiler.
+ */
 function init() {
+	//Initialize Ace embedded code editor
 	var editor = ace.edit("input");
 	editor.setTheme("ace/theme/chrome");
 	editor.getSession().setMode("ace/mode/javascript");
 	
 	$("#compile").on("click", function(){
-		$("#output").text("");
+		input = editor.getValue() + " ";
+		tokens = [];
+		clearOutput();
+		printOutput("Test output");
 	});
+}
+
+/**
+ * Clears text output from output screen.
+ */
+function clearOutput() {
+	$("#output").text("");
+}
+
+/**
+ * Prints text output to output screen.
+ *
+ * @param {string} o Text output.
+ */
+function printOutput(o) {
+	$("#output").append(o + "<br />\n");
 }
--- scripts/lexer.js
+++ scripts/lexer.js
@@ -9,3 +9,44 @@ function lexer() {
 function nextToken() {
 	
 }
+
+function idToken(s) {
+	s = s.trim(); //Remove excess whitespace
+	if (s.length === 0) return true;
+	
+	switch (s) {
+		case '{':
+			//T_LBRACE
+		case '}':
+			//T_RBRACE
+		case '(':
+			//T_LPAREN
+		case ')':
+			//T_RPAREN
+		case '=':
+			//T_ASSIGN
+		case '==':
+		case '!=':
+			//T_BOOLOP
+		case '$':
+			//T_EOF
+		case '+':
+			//T_OPERATOR
+		case '"':
+			//T_QUOTE
+		case 'false':
+		case 'true':
+			//T_BOOLEAN
+		case 'print':
+		case 'while':
+		case 'if':
+			//T_KEYWORD
+		case 'int':
+		case 'string':
+		case 'boolean':
+			//T_TYPE
+		default:
+			break;
+	}
+	
+}
