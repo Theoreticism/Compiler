bc215778eb90f3f4967ffb24df9e6d12ef761f4b
chriskwlee <ckwl.default@gmail.com>
2015-02-25 04:38:18 -0500

    Parser framework
    Parser framework in place, first commit.

--- scripts/parser.js
+++ scripts/parser.js
@@ -1,3 +1,109 @@
 function parser() {
 	
 }
+
+function parseProgram() {
+
+}
+
+function parseBlock() {
+
+}
+
+function parseStatementList() {
+
+}
+
+function parseStatement() {
+	if () {
+		parsePrintStatement();
+	} else if () {
+		parseAssignmentStatement();
+	} else if () {
+		parseVarDecl();
+	} else if () {
+		parseWhileStatement();
+	} else if () {
+		parseIfStatement();
+	} else if () {
+		parseBlock();
+	}
+}
+
+function parsePrintStatement() {
+
+}
+
+function parseAssignmentStatement() {
+
+}
+
+function parseVarDecl() {
+
+}
+
+function parseWhileStatement() {
+
+}
+
+function parseIfStatement() {
+
+}
+
+function parseExpr() {
+	if () {
+		parseIntExpr();
+	} else if () {
+		parseStringExpr();
+	} else if () {
+		parseBooleanExpr();
+	}
+}
+
+function parseIntExpr() {
+
+}
+
+function parseStringExpr() {
+
+}
+
+function parseBooleanExpr() {
+
+}
+
+function parseId() {
+
+}
+
+function parseCharList() {
+
+}
+
+function parseType() {
+
+}
+
+function parseChar() {
+
+}
+
+function parseSpace() {
+
+}
+
+function parseDigit() {
+
+}
+
+function parseBoolop() {
+
+}
+
+function parseBoolval() {
+
+}
+
+function parsePlus() {
+
+}
