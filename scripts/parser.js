e691b764113e88bcdcf224a98d4118fb142e595d
chriskwlee <ckwl.default@gmail.com>
2015-02-26 04:52:24 -0500

    Parse function
    Parse function in place for future CST requirements (no work done on CST
    yet). Token check function framework in place.

--- scripts/parser.js
+++ scripts/parser.js
@@ -1,17 +1,37 @@
 var currentToken;
 var tokenIndex;
 
-function getNextToken() {
+function parser() {
+	tokenIndex = 0;
+	currentToken = getNextToken();
+	parseProgram();
+	//TODO: CST stuff
+}
+
+function getNext() {
 	if (tokenIndex < tokens.length) {
 		return tokens[tokenIndex++];
 	}
 }
 
 function checkToken(cToken) {
+
+	//Expected token type
 	if (cToken != "T_RBrace") {
 		printOutput("Expecting a {0}".format(cToken));
 	} else {
 		printOutput("Expecting a {0} or a statement".format(cToken));
+	}
+	
+	//Acquired token type
+	if (currentToken.type == cToken) { //CHECK THIS
+		printOutput("Got a {0}".format(cToken));
+	} else {
+		//Output parse error
+	}
+	
+	if (currentToken.type != "T_EOF") {
+		currentToken = getNext();
 	}
 }
 
